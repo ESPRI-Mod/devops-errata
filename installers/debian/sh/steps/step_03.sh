affe33f6eb5ee518234d86057f60eb94eaadc19b
AtefBN <abennasser@ipsl.fr>
2023-11-07 18:24:45 +0100

    Reverted installing pyenv python version 2

--- installers/debian/sh/steps/step_03.sh
+++ installers/debian/sh/steps/step_03.sh
@@ -8,12 +8,13 @@ main()
 {
     log "BEGIN step 3:"
 
-    log "... step 3.1: initialising python 3"
-    init_python $INSTALLER_PYTHON_3
+    log "... step 3.1: initialising python 2"
     init_python $INSTALLER_PYTHON_2
+
+    log "... step 3.2: initialising python 3"
+    init_python $INSTALLER_PYTHON_3
 
     log "END step 3"
 }
-
 # Invoke entry point.
 main
